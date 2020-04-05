b85ec5bb63976c2758c190241ec702ef3b6e988f
adamcl <adamlil@kth.se>
2020-04-05 10:22:42 +0200

    test

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const dayOfTheWeek = (date = new Date()) => {
     const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
     return days[date.getDay()];
-}
+};
 
 
 try {
