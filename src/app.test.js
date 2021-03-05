4bb8f08116e7b33d78c56874a99946a239528459
orhankalayci-devopscu <orhan@kalayci.com>
2021-03-05 13:12:14 +0300

    US calender seems to be used

--- src/app.test.js
+++ src/app.test.js
@@ -2,6 +2,6 @@ const { dayOfTheWeek } = require('./app');
 
 
 test('getDay returns the long-format day of the week', () => {
-    const day = dayOfTheWeek( new Date('5/3/2021') );
+    const day = dayOfTheWeek( new Date('3/5/2021') );
     expect( day ).toBe('Friday');
 });
