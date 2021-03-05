208a26160c23c6fb07ff2c00887da5ef0163049d
orhankalayci-devopscu <orhan@kalayci.com>
2021-03-05 12:56:46 +0300

    to fail the test make a typo on wednesday

--- src/app.test.js
+++ src/app.test.js
@@ -3,5 +3,5 @@ const { dayOfTheWeek } = require('./app');
 
 test('getDay returns the long-format day of the week', () => {
     const day = dayOfTheWeek( new Date('3/11/2020') );
-    expect( day ).toBe('Wednesday');
+    expect( day ).toBe('Wdnesday');
 });
