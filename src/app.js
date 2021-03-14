0c5e3856ad0d0812dfc708cadc01684939f28992
Scuftz <shivneel.singh1600@gmail.com>
2021-03-14 16:31:00 +1300

    failed test code

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const dayOfTheWeek = (date = new Date()) => {
-    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const days = ['Snday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
     return days[date.getDay()];
 }
