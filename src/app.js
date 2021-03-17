b04b833c08871f09d52200bad9992d89ab80e1e3
Chris Stehlin <castehlin@gmail.com>
2021-03-18 00:06:57 +1300

    now on feature branch, hopefully broke it

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const dayOfTheWeek = (date = new Date()) => {
 
 
 try {
-    document.getElementById('day').innerText = dayOfTheWeek();
+    document.getElemfafentById('day').innerText = dayOfTheWeek();
 } catch(err) {}
 
 
