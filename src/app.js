edb4bf10087c303e1ce2433ff4097d900e983997
signatureratings <balusairam26@gmail.com>
2021-05-27 12:18:45 +0530

    added year and month

--- src/app.js
+++ src/app.js
@@ -1,13 +1,35 @@
 const dayOfTheWeek = (date = new Date()) => {
-    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-
-    return days[date.getDay()];
+  const days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ]
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ]
+  let arr = [days[date.getDay()], months[date.getMonth()], date.getFullYear()]
+  return arr
 }
 
-
 try {
-    document.getElementById('day').innerText = dayOfTheWeek();
-} catch(err) {}
+  document.getElementById('day').innerText = dayOfTheWeek()[0]
+  document.getElementById('month').innerText = dayOfTheWeek()[1]
+  document.getElementById('year').innerText = dayOfTheWeek()[2]
+} catch (err) {}
 
-
-exports.dayOfTheWeek = dayOfTheWeek;
+exports.dayOfTheWeek = dayOfTheWeek
