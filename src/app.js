3b2b2724565ec04b9f4235482a6e6b72f814756f
Leonel Velasquez <leonel@zauru.com>
2023-01-17 21:29:27 -0600

    First commit

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const dayOfTheWeek = (date = new Date()) => {
     const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-
+    //comment
     return days[date.getDay()];
 }
 
