b3fd618bcd6289a3576eb6ae44359ef208933877
ChanWoo <everpresent@hanmail.net>
2023-02-26 17:23:00 +0900

    firebase

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const dayOfTheWeek = (date = new Date()) => {
-    const days = ['Sunday', 'Monday', 'Tuesday', 'Wesday', 'Thursday', 'Friday', 'Saturday'];
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
     return days[date.getDay()];
 }
