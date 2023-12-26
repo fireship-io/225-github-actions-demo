6e790ba948d5e930ba23326c94315ada2da7b2d3
hienngm <hienngm99@gmail.com>
2023-12-27 00:14:49 +0700

    test

--- src/app.test.js
+++ src/app.test.js
@@ -1,7 +1,6 @@
-const { dayOfTheWeek } = require('./app');
+const { dayOfTheWeek } = require("./app");
 
-
-test('getDay returns the long-format day of the week', () => {
-    const day = dayOfTheWeek( new Date('3/11/2020') );
-    expect( day ).toBe('Wednesday');
+test("getDay returns the long-format day of the week", () => {
+  const day = dayOfTheWeek(new Date("2/11/2020"));
+  expect(day).toBe("Wednesday");
 });
