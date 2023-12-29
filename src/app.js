b17885795e03eab78e88356610908aaeb58c60da
Raymond Hebard <rayhebard@yahoo.com>
2023-12-29 14:40:10 -0500

    Clean up spacing

--- src/app.js
+++ src/app.js
@@ -1,13 +1,19 @@
 const dayOfTheWeek = (date = new Date()) => {
-    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
 
-    return days[date.getDay()];
-}
-
+  return days[date.getDay()];
+};
 
 try {
-    document.getElementById('day').innerText = dayOfTheWeek();
-} catch(err) {}
-
+  document.getElementById("day").innerText = dayOfTheWeek();
+} catch (err) {}
 
 exports.dayOfTheWeek = dayOfTheWeek;
